e1cbfe8344a2c87b214f692c8781e905f702b2ed
Vivek <vvivksingh@gmail.com>
2021-12-21 11:30:07 +0530

    Finding reverse of given number

---
+++ ReverseOfNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ReverseOfNumber{
+//method for reverse a number
+	public static void reverseNumber(int number){
+		if (number < 10){
+			System.out.println(number);
+			return;
+		}
+		else{
+			System.out.print(number % 10);
+			reverseNumber(number/10);
+		}
+	}
+	public static void main(String args[]){
+		System.out.print("Enter the number that you want to reverse: ");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		System.out.print("The reverse of the given number is: ");
+		//method calling
+		reverseNumber(num);
+	}
+}
